f8f415028653abb20b76bc5d3a1f9255464c4514
Gu1zGu1z <117632588+Gu1zGu1z@users.noreply.github.com>
2023-01-16 08:59:11 +0100

    Update and rename jeux.py to jeux1.py

--- jeux1.py
+++ jeux1.py
@@ -15,6 +15,25 @@ pyxel.init(config['taille_x'], config['taille_y'], title=config['titre'])
 
 
 
+# ----------- Balle --------------------------
+# position initiale de la balle
+# (origine des positions : milieu du tiers inférieur)
+balle_x = 35
+balle_y = 100
+
+
+def balle_deplacement(x, y):
+    """déplacement avec les touches de directions verticales"""
+     if pyxel.btn(pyxel.KEY_DOWN):
+        if (y < 120) :
+            y = y + 20
+    if pyxel.btn(pyxel.KEY_UP):
+        if (y > 0) :
+            y = y - 20
+    return x, y
+
+
+
 
 
 
@@ -22,7 +41,11 @@ pyxel.init(config['taille_x'], config['taille_y'], title=config['titre'])
 # == UPDATE
 # =========================================================
 def update():
-  pass()
+   """mise à jour des variables (30 fois par seconde)"""
+
+    global balle_x, balle_y
+    # mise à jour de la position du plateau
+    balle_x, balle_y = balle_deplacement(balle_x, balle_y)
 
 
 # =========================================================
@@ -35,6 +58,9 @@ def draw():
 
     # vide la fenetre
     pyxel.cls(0)
+    
+    # balle (cercle)
+        pyxel.circ(balle_x, balle_y, config['rayon_balle'], 2)
     
 # https://kitao.github.io/pyxel/wasm/launcher/?run=Gu1zGu1z.Projet.jeux
     
