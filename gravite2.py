3dc859ee66d7f6212448a8cac04ca12e48fdc9c2
Gu1zGu1z <117632588+Gu1zGu1z@users.noreply.github.com>
2023-01-30 08:30:08 +0100

    Update and rename gravite1.py to gravite2.py

--- gravite2.py
+++ gravite2.py
@@ -1,5 +1,20 @@
 import pyxel
 
+
+# définit les différentes variables
+config = { 
+    'taille_x' : 152, \
+    'taille_y' : 152, \
+    'titre' : 'flip flop', \
+    'rayon_balle': 2, \
+    'vitesse_max' : 15, \
+    'niveau_max' : 3,
+    'score' : 0, \
+    'balle_max' : 120, \
+    'balle_min' : 37
+}
+
+
 class Ball:
     def __init__(self, x, y, gravite):
         self.x = x
@@ -16,16 +31,16 @@ class Game:
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_UP):
-            self.gravite = -1
+            self.gravite = -3
         elif pyxel.btnp(pyxel.KEY_DOWN):
-            self.gravite = 1
+            self.gravite = 3
 
         self.ball.gravite = self.gravite
         self.ball.update()
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.circ(self.ball.x, self.ball.y, 5, 9)
+        pyxel.circ(self.ball.x, self.ball.y, config['rayon_balle'], 2, 9)
 
 # Initialize Pyxel
 pyxel.init(160, 120)
