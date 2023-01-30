40cb3ee79278f73a09a8f0bd97911757a36fef0e
Gu1zGu1z <117632588+Gu1zGu1z@users.noreply.github.com>
2023-01-30 09:45:08 +0100

    Update and rename chrono2.py to chrono3.py

--- chrono3.py
+++ chrono3.py
@@ -9,35 +9,39 @@ class Chronometer:
         self.time = 0
         self.running = True
 
+    def switch(self):
+        self.running = not self.running
+        
     def stop(self):
         self.running = False
 
     def update(self):
         if pyxel.btnr(pyxel.KEY_A):
-            self.running = not self.running
+            self.switch()
         if self.running:
             self.time += 1
 
     def draw(self):
         minutes, seconds = self.time//(30*60), (self.time//30)%60
-        pyxel.text(135, 5, f"{minutes:02}{seconds:02}", 7)
+        pyxel.text(135, 5,f"{minutes:02}:{seconds:02}", 7)
 
 class Jeu():
     def __init__(self):
 # Initialise Pyxel
-    pyxel.init(160, 120)
+        pyxel.init(160, 120)
 
 # Create an instance of the chronometer
-    self.chronometer = Chronometer()
+        self.chronometer = Chronometer()
 
 # Start the chronometer
-    self.chronometer.start()
-    pyxel.run(self.update, self.draw)
+        self.chronometer.start()
+        pyxel.run(self.update, self.draw)
 
 
 # Update function for Pyxel
     def update(self):
-        chronometer.update()
+        self.chronometer.update()
+        
 
 # Draw function for Pyxel
     def draw(self):
