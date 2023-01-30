212040eb27d21e4677136f4d4174c0b409983f67
Gu1zGu1z <117632588+Gu1zGu1z@users.noreply.github.com>
2023-01-30 09:48:52 +0100

    Create score1.py

---
+++ score1.py
@@ -0,0 +1,52 @@
+import pyxel
+
+class Score:
+    def __init__(self):
+        self.time = 0
+        self.running = False
+
+    def start(self):
+        self.time = 0
+        self.running = True
+
+    def switch(self):
+        self.running = not self.running
+        
+    def stop(self):
+        self.running = False
+
+    def update(self):
+        if pyxel.btnr(pyxel.KEY_A):
+            self.switch()
+        if self.running:
+            self.time += 1
+
+    def draw(self):
+        seconds = self.time//60
+        pyxel.text(135, 5,f"{seconds:08}", 7)
+
+class Jeu():
+    def __init__(self):
+# Initialise Pyxel
+        pyxel.init(160, 120)
+
+# Create an instance of the chronometer
+        self.score = Score()
+
+# Start the chronometer
+        self.score.start()
+        pyxel.run(self.update, self.draw)
+
+
+# Update function for Pyxel
+    def update(self):
+        self.score.update()
+        
+
+# Draw function for Pyxel
+    def draw(self):
+        pyxel.cls(0)
+        self.score.draw()
+
+# Run Pyxel
+Jeu()
