6b12843efc35178e190939cf0a4cb62350a77bf6
Gu1zGu1z <117632588+Gu1zGu1z@users.noreply.github.com>
2023-03-16 11:30:11 +0100

    Update and rename jeux64.py to jeux65.py

--- jeux65.py
+++ jeux65.py
@@ -100,8 +100,8 @@ class Jeu:
             # la balle est donc attirée vers le bas puisque self.y tend vers +∞
 
         # generation d'un nouvel ennemi tous les 50 "frames"
-        nombre = random.randint(200, 220)
-        if pyxel.frame_count % nombre == 0:
+        #nombre = random.randint(200, 220)
+        if pyxel.frame_count % 200 == 0:
             self.ennemis.append(Ennemi())
 
         for ennemi in self.ennemis:
