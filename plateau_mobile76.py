0c18171fb576d12c78f5b86d6c91519633fb2577
margotgermond <117632361+margotgermond@users.noreply.github.com>
2023-05-07 18:01:05 +0200

    Update and rename plateau_mobile75.py to plateau_mobile76.py

--- plateau_mobile76.py
+++ plateau_mobile76.py
@@ -22,7 +22,7 @@ config = {
     'score' : 0, \
     'balle_max' : TAILLE_FENETRE_H, \
     'balle_min' : 37, \
-    'gravite' : 2, \
+    'gravite' : 3, \
     'bande1_y' : 50, \
     'bande2_y' : 105, \
     'vitesse_ennemis' : 2, \
@@ -65,7 +65,7 @@ class Niveau:
         if self.chrono.time > self.chrono_precedent and (self.chrono.time % config['frequence_niveau']) == 0:
             self.niveau += 1
             # change de niveau
-            #self.gravité += 1
+            self.gravite += 1
             # incremente la vitesse de gravite de la balle a chaque niveau
             # noter chrono_precedent est utile sinon on n'arrive jamais à sortir du pause level up
             # parce qu'on retombe direct dedans
@@ -79,8 +79,7 @@ class Niveau:
             else:
                 self.bande1.vitesse += 1
                 self.bande2.vitesse += 1
-                self.gravité += 1
-                # incremente la vitesse de gravite de la balle a chaque niveau
+                
 
     def draw(self):
       # affichage du numéro du niveau sur la meme ligne que le score (chrono)
